f1170137403c98c916110485d0f7cceb977058fd
manni1023 <116977180+manni1023@users.noreply.github.com>
2024-10-19 14:19:14 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 1443456   //cannot be changed
+let accountEmail = "javvajimanvitha@gmail.com"
+var accPass = "mann"
+accountCity = "jaipur" //we cannot use like this  it can be written but 
+let accountState;
+//accountId=2 it is not allowed
+accountEmail="manni@gmail.com"
+accPass="mannii"
+accountCity="hyd"
+console.log(accountId);
+/* 
+Prefer not to use war cz of issue in block scope and functional scope 
+*/
+console.table([accountEmail,accPass,accountCity,accountState])
+
