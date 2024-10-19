7e0f5dc2c2b9198a25fd40191daae72d5e42773d
manni1023 <116977180+manni1023@users.noreply.github.com>
2024-10-19 18:27:52 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict";   //treat the js code as newer code or version 
+
+//alert(3+3) // we are using node js not browser
+
+console.log(3+3) 
+       //code redability should be high we cannot write as we wish if we want although it works
+ console.log("manni")
+ //we can study standard thing in ecma **
+
+
+let name ="manni"   //string
+let age = 18    //int
+let isLoggedin =  true  //boolean
+let state;
+
+/*datatypes
+number - 2 to powe 53
+bigint
+string --> ""
+boolean true or false
+null => standlone value 
+undefined -> value is not defined null is a represntation of value is not there
+symbol --> unique
+*/
+
+//object 
+console.log(typeof undefined);  //undefined
+console.log(typeof null);  //object
+
