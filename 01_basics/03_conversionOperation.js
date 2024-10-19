ca72e07f59d1999119d809b4e26ad37a528ae0cb
manni1023 <116977180+manni1023@users.noreply.github.com>
2024-10-19 19:30:54 +0000

    conversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = true
 
-console.log(typeof score);  //direct
-console.log(typeof (score));  //function
+//console.log(typeof score);  //direct
+//console.log(typeof (score));  //function
 
 let valueInNum = Number(score) //to convert in number or to convert fro one type to another
-console.log(typeof valueInNum);  //direct
-console.log(valueInNum)  //--> "33abc will be converted but actual is NaN it is cz it cant be convertd"
+//console.log(typeof valueInNum);  //direct
+//console.log(valueInNum)  //--> "33abc will be converted but actual is NaN it is cz it cant be convertd"
 //"33" => 33
 //"33abc" => Nan not a number
 // true => 1; false = 0
@@ -14,12 +14,38 @@ let isLoggedIn ="manni"
 
 let boolisLoggedin = Boolean(isLoggedIn)
 
-console.log(boolisLoggedin);
+//console.log(boolisLoggedin);
 // 1=>truue ;0=>false
 //"" => false
 // "manni" => true
 
 let someNum= 33
 let strNum =  String(someNum)
-console.log(strNum)
-console.log(typeof strNum);
+//console.log(strNum)
+//console.log(typeof strNum);
+
+// Operations 
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2); //power
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 ="hello"
+let str2 = " manni"
+
+let str3 = str1+str2
+//console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(true )
