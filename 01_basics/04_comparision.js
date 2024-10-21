fee88408863eb629d137e6b308761081ccbab663
manni1023 <116977180+manni1023@users.noreply.github.com>
2024-10-21 16:08:23 +0000

    comparison

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,15 @@
+// console.log(2>1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+//  console.log(null>0);
+//  console.log(null==0);   in these it may caouse confusion and we should avoid
+//  console.log(null>=0);
+
+// console.log(undefined==0);
+//  console.log(undefined<0);
+// console.log(undefined>=0);
+
+//strict check  ===  not onlyvales it wioll check data types also
+
