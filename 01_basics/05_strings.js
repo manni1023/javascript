2dfcf22fdf28a371f5ec32d44b64052ddb7fc0d6
manni1023 <116977180+manni1023@users.noreply.github.com>
2024-10-23 15:32:50 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,18 @@
+const name ="manni"
+const repocount = 50 
+ 
+// console.log(name + repocount + " value");    //outdated
+
+//now we use bacticks use is string interpolation
+
+//console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+
+const gameName = new String('manni')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);   //to get prototype
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('n'));
